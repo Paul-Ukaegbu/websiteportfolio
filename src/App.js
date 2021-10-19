8ef993829b813d56babbb366e716bbb858fd94e1
Paul Ukaegbu <paulukaegbu@devpapi.io>
2021-10-19 19:01:30 -0400

    Added about me, and image to body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import logo from './github2.png';
+import github from './github5.png';
+import linkedin from './linkedin.png';
+import dev from './dev2.png';
+import dev3 from './dev3.png';
 
 function App() {
   return (
@@ -9,16 +12,29 @@ function App() {
     <body>
       <div className = "nav">
         <h2>DEVPAPI</h2>
+        {/* <img className="navlinks" src={github}/>
+        <img className="navlinks" src={linkedin}/> */}
       </div>
-      <h5>Website coming soon..</h5>
+
+      <div className="container text-center">
+        <div className="row">
+          <div id= "aboutMe" className="col">
+            <p className= "aboutMe">Hi! my name is <span>Paul Ukaegbu</span> and I am a full-stack developer.</p>
+          </div>
+          <div className="col">
+            <img className="images" src={dev3}/>
+          </div>
+        </div>  
+      </div>
+
       <h4>Projects</h4>
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-        <div class="card h-100">
-          {/* <img src="..." class="card-img-top" alt="..."> */}
-          <div class="card-body">
-            <h5 class="card-title">Chat App</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+        <div className="card h-100">
+          <img src={dev} class="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Chat App</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <a href="#" class="btn btn-primary">Check Me Out</a>
           </div>
           <div class="card-footer">
@@ -28,7 +44,7 @@ function App() {
       </div>
       <div class="col">
         <div class="card h-100">
-          {/* <img src="..." class="card-img-top" alt="..."> */}
+          <img src={dev} class="card-img-top" alt="..."/>
           <div class="card-body">
             <h5 class="card-title">Calculator App</h5>
             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -41,7 +57,7 @@ function App() {
       </div>
       <div class="col">
         <div class="card h-100">
-          {/* <img src="..." class="card-img-top" alt="..."> */}
+          <img src={dev} class="card-img-top" alt="..."/>
           <div class="card-body">
             <h5 class="card-title">Classroom App</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>

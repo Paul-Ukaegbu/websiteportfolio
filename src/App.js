37e815246756e01eb6c16b831dbd5a1a034c5237
Paul Ukaegbu <paulukaegbu@devpapi.io>
2021-10-21 02:53:51 -0400

    made mobile friendly

--- src/App.js
+++ src/App.js
@@ -9,68 +9,81 @@ function App() {
   return (
     <div className="App">
     <body>
+
       <div className = "nav">
         <h2>DEVPAPI</h2>
       </div>
 
+      <div className = "body">
       <div className="container text-center">
         <div className="row">
           <div id= "aboutMe" className="col">
-            <p className= "aboutMe">Hi! my name is <span>Paul Ukaegbu</span> and I am a <span>full-stack developer.</span></p>
-            <img className="navlinks" src={github}/>
-            <img className="navlinks" src={linkedin}/>
+            <p className= "aboutMe">Hi! My name is <span>Paul Ukaegbu</span> and I am a <span>full-stack developer.</span></p>
+            {/* <img className="navlinks" src={github}/>
+            <img className="navlinks" src={linkedin}/> */}
+            <button type="button" class="btn btn-outline-info"><a href={"https://www.linkedin.com/in/paulukaegbu/"} target="_blank">Linkedin</a></button>
+            <button type="button" class="btn btn-outline-info"><a href={"https://github.com/Paul-Ukaegbu"} target="_blank">Github</a></button>            
           </div>
           <div className="col">
             <img className="images" src={dev3}/>
           </div>
         </div>  
       </div>
+      </div>
+        
+        <div className="bodyMobile">
+          <div className = "navMobile">
+            <h2>DEVPAPI</h2>
+          </div>
+          <img className="images" src={dev}/>
+          <p className= "aboutMe">Hi! My name is <span>Paul Ukaegbu</span> and I am a <span>full-stack developer.</span></p>
 
-      <h4>Projects</h4>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        <div className="col">
-        <div className="card h-100">
-          <img src={dev} class="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">Chat App</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <a href="#" class="btn btn-primary">Check Me Out</a>
+        </div>
+
+        <h4>Projects</h4>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="col">
+          <div className="card h-100">
+            <img src={dev} class="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Chat App</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="#" class="btn btn-primary">Check Me Out</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Python, Django, MySql, HTML, CSS</small>
+            </div>
           </div>
-          <div class="card-footer">
-            <small class="text-muted">Python, Django, MySql, HTML, CSS</small>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src={dev} class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Calculator App</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Check Me Out</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">C#, .NET, MySql </small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src={dev} class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Classroom App</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <a href="#" class="btn btn-primary">Check Me Out</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">JAVA</small>
+            </div>
           </div>
         </div>
       </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src={dev} class="card-img-top" alt="..."/>
-          <div class="card-body">
-            <h5 class="card-title">Calculator App</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Check Me Out</a>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">C#, .NET, MySql </small>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src={dev} class="card-img-top" alt="..."/>
-          <div class="card-body">
-            <h5 class="card-title">Classroom App</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <a href="#" class="btn btn-primary">Check Me Out</a>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">JAVA</small>
-          </div>
-        </div>
-      </div>
-</div>
+
     </body>
-
-
     </div>
 
 
